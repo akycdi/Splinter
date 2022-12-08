13f9a3c99aab08eabb1cd44c90403cf9cdcfe3f6
arunkumar.b <arunkumar.b@holosuit.com>
2022-12-08 22:27:22 +0530

    WIP

--- Splinter/ViewModels/BaseViewModel.cs
+++ Splinter/ViewModels/BaseViewModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Splinter.ViewModels
+﻿namespace Splinter.ViewModels
 {
     internal class BaseViewModel
     {
