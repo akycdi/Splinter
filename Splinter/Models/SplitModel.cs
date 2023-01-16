4fe59b6cf2ceec62524e4a55e9341eb201712468
arun <arun36824@gmail.com>
2023-01-16 10:44:54 +0530

    WIP

---
+++ Splinter/Models/SplitModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Splinter.Models
+{
+    public class SplitModel
+    {
+        string SpliterName;
+        public string Name { get { return name; } }
+    }
+}
